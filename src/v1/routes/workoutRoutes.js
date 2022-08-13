65111516ec54c79e3acfcb64d345e87237c92cdf
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-13 18:50:42 -0500

    Mejorando API

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const router = express.Router()
 const workoutController = require("../../controllers/workoutController")
-
+const recordController = require("../../controllers/recordController")
 
 router
   .get('/', workoutController.getAllWorkouts)
 
   .get("/:workoutId", workoutController.getOneWorkout)
 
+  .get("/:workoutId/records", recordController.getRecordForWorkout)
+
   .post("/", workoutController.createNewWorkout)
 
   .patch("/:workoutId", workoutController.updateOneWorkout)
 
-  .delete("/:workoutId", (workoutController.deleteOneWorkout))
+  .delete("/:workoutId", workoutController.deleteOneWorkout)
 
 module.exports = router
