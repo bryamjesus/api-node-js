706ef97ffa9ab64d642e64ae8dc26a227f356430
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-13 12:35:02 -0500

    Agregando versión a las apis

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const v1Router = require("./v1/routes")
+const v2Router = require("./v2/routes")
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-  res.send('<h1>Hola Wordl</h1>')
-})
+app.use("/api/v1", v1Router)
+app.use("/api/v2", v2Router)
+
+// app.get('/',(req,res)=>{
+//   res.send('<h1>Hola Wordl</h1>')
+// })
 
 app.listen(PORT, ()=>{console.log(`🚀 Server listening on port ${PORT}`)})
