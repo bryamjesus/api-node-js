79749f5bbd35f09ac65d591cb2a1c9dd068faf08
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-13 13:19:32 -0500

    Añadiendo controladores

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const v1Router = require("./v1/routes")
+// const v1Router = require("./v1/routes")
 const v2Router = require("./v2/routes")
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1", v1Router)
-app.use("/api/v2", v2Router)
-
 // app.get('/',(req,res)=>{
 //   res.send('<h1>Hola Wordl</h1>')
 // })
+
+// app.use("/api/v1", v1Router)
+app.use("/api/v2", v2Router)
+
+app.use("/api/v1/workouts", v1WorkoutRouter)
 
 app.listen(PORT, ()=>{console.log(`🚀 Server listening on port ${PORT}`)})
