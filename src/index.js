032666df3ba358c93dadd8c66ff2f967ddc35cac
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-13 19:34:08 -0500

    Documentacion de la API - Swagger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require('express')
+const apicache = require("apicache")
 // const v1Router = require("./v1/routes")
 // const v2Router = require("./v2/routes")
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger")
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
+const cache = apicache.middleware
 
 // app.get('/',(req,res)=>{
 //   res.send('<h1>Hola Wordl</h1>')
@@ -14,6 +17,10 @@ const PORT = process.env.PORT || 3000;
 // app.use("/api/v2", v2Router)
 
 app.use(express.json())
+app.use(cache("2 minutes"))
 app.use("/api/v1/workouts", v1WorkoutRouter)
 
-app.listen(PORT, () => { console.log(`🚀 Server listening on port ${PORT}`) })
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on port ${PORT}`)
+  V1SwaggerDocs(app, PORT)
+})
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,59 @@
-const express = require("express")
-const router = express.Router()
 const workoutController = require("../../controllers/workoutController")
 const recordController = require("../../controllers/recordController")
+// const authenticate = require("../../middlewares/authenticate");
+// const authorize = require("../../middlewares/authorize");
 
+const express = require("express")
+const apicache = require("apicache")
+const router = express.Router()
+const cache = apicache.middleware;
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router
-  .get('/', workoutController.getAllWorkouts)
+  .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
 
   .get("/:workoutId", workoutController.getOneWorkout)
 
